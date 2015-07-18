9154c517352e31df801f99a9c567846256bc7c7e
Taapat <taapat@gmail.com>
2015-07-18 20:31:31 +0300

    Style changes in setup

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ from distutils.core import setup
 import setup_translate
 
 
-setup (name = 'enigma2-plugin-extensions-youtube',
-	version='1.0',
-	author='Taapat',
-	author_email='taapat@gmail.com',
-	package_dir = {'Extensions.YouTube': 'src'},
-	packages=['Extensions.YouTube'],
-	package_data={'Extensions.YouTube': ['*.png', 'icons/*.png']},
-	description = 'Watch YouTube videos',
-	cmdclass = setup_translate.cmdclass,
-)
+setup(name = 'enigma2-plugin-extensions-youtube',
+		version='1.0',
+		author='Taapat',
+		author_email='taapat@gmail.com',
+		package_dir = {'Extensions.YouTube': 'src'},
+		packages=['Extensions.YouTube'],
+		package_data={'Extensions.YouTube': ['*.png', 'icons/*.png']},
+		description = 'Watch YouTube videos',
+		cmdclass = setup_translate.cmdclass,
+	)
+
