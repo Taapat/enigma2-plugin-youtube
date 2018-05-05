35f93c1472b04e4aedaef592674244717091c756
Taapat <taapat@gmail.com>
2018-05-05 19:26:19 +0300

    YouTubeVirtualKeyBoard: try fix GSOD if youtube search is not available
    This is try to fix https://github.com/Taapat/enigma2-plugin-youtube/issues/35
    I have not able to repeat this, but it seems that if youtube search in the web is not available, then the suggestions are delayed and updateSuggestions are called after closing the YouTubeVirtualKeyBoard window.
    I hope this could fix this error.

--- src/YouTubeSearch.py
+++ src/YouTubeSearch.py
@@ -37,10 +37,11 @@ class YouTubeVirtualKeyBoard(VirtualKeyBoard):
 			self.searchValue.getSuggestions()
 
 	def updateSuggestions(self, suggestions):
-		if len(suggestions) > 1:
-			self['header'].setText(', '.join(x[0] for x in suggestions[1:]))
-		else:
-			self['header'].setText('')
+		if 'header' in self:
+			if len(suggestions) > 1:
+				self['header'].setText(', '.join(x[0] for x in suggestions[1:]))
+			else:
+				self['header'].setText('')
 
 
 class YouTubeSearch(Screen, ConfigListScreen):
