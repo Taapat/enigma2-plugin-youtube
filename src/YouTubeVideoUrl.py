df32af0f2336f02325ee3ef9845ec6713b0365e0
Taapat <taapat@gmail.com>
2019-01-20 13:52:19 +0200

    YouTubeVideoUrl: extend JS player signature function name regexes, thx dstftw
    Based on: https://github.com/rg3/youtube-dl/commit/fa4ac365f69cbd51e4c9801984ebea49a12825b7

--- src/YouTubeVideoUrl.py
+++ src/YouTubeVideoUrl.py
@@ -216,8 +216,8 @@ class YouTubeVideoUrl():
 		funcname = self._search_regex(
 				(r'(["\'])signature\1\s*,\s*(?P<sig>[a-zA-Z0-9$]+)\(',
 				r'\.sig\|\|(?P<sig>[a-zA-Z0-9$]+)\(',
-				r'yt\.akamaized\.net/\)\s*\|\|\s*.*?\s*c\s*&&\s*d\.set\([^,]+\s*,\s*(?P<sig>[a-zA-Z0-9$]+)\(',
-				r'\bc\s*&&\s*d\.set\([^,]+\s*,\s*(?P<sig>[a-zA-Z0-9$]+)\(',
+				r'yt\.akamaized\.net/\)\s*\|\|\s*.*?\s*c\s*&&\s*d\.set\([^,]+\s*,\s*(?:encodeURIComponent\s*\()?(?P<sig>[a-zA-Z0-9$]+)\(',
+				r'\bc\s*&&\s*d\.set\([^,]+\s*,\s*(?:encodeURIComponent\s*\()?\s*(?P<sig>[a-zA-Z0-9$]+)\(',
 				r'\bc\s*&&\s*d\.set\([^,]+\s*,\s*\([^)]*\)\s*\(\s*(?P<sig>[a-zA-Z0-9$]+)\('),
 				jscode, group='sig')
 		jsi = JSInterpreter(jscode)
