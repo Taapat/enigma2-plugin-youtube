8ee8d2905c6fc67f913f3d80a8b85b02d30d868c
Taapat <taapat@gmail.com>
2020-05-02 16:49:10 +0300

    travis_fix.sh: use search region and language

--- travis_fix.sh
+++ travis_fix.sh
@@ -3,5 +3,5 @@
 cp -f travis_init.py ./src/__init__.py
 sed -i 's/from Components/#from Components/g' ./src/YouTubeVideoUrl.py
 sed -i 's/config.plugins.YouTube.maxResolution.value/"22"/g' ./src/YouTubeVideoUrl.py
-sed -i 's/config.plugins.YouTube.searchRegion.value/""/g' ./src/YouTubeVideoUrl.py
-sed -i 's/config.plugins.YouTube.searchLanguage.value/""/g' ./src/YouTubeVideoUrl.py
+sed -i 's/config.plugins.YouTube.searchRegion.value/"US"/g' ./src/YouTubeVideoUrl.py
+sed -i 's/config.plugins.YouTube.searchLanguage.value/"en"/g' ./src/YouTubeVideoUrl.py
