1f8bc35b1794898d3dc23cff3d98660e561262fd
Taapat <taapat@gmail.com>
2020-05-10 14:16:43 +0300

    test_plugin: try to fix false travis build reports
    Looks like the videos in ello playlists have some regional limitations.

--- test_plugin.py
+++ test_plugin.py
@@ -110,7 +110,7 @@ def test_mostViewedFeeds():
 	CheckExample(q='', eventType='', order='viewCount', descr='Most Viewed')
 
 def test_playlist():
-	CheckExample(q='ello', eventType='', order='relevance', s_type='playlist', descr='Playlist')
+	CheckExample(q='youtube', eventType='', order='relevance', s_type='playlist', descr='Playlist')
 
 
 _TESTS = [
