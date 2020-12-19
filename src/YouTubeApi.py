Taapat/enigma2-plugin-youtube

commit 87aa502a0c2a3b8486f070fe6bba602043b91491

YouTubeApi: fix access token renew, thx for report mop80
urllib2 raise HTTP Error on Unauthorized acess.
So neet to use except for HTTPError to get error code 401 for access token renew.

src/YouTubeApi.py
@@ -6,6 +6,7 @@ from .compat import compat_ssl_urlopen
 from .compat import compat_quote
 from .compat import compat_urlopen
 from .compat import compat_Request
+from .compat import compat_HTTPError
 
 
 def GetKey(x):
@@ -46,65 +47,79 @@ class YouTubeApi:
 		if self.access_token:
 			self.key = self.key + '&access_token=' + self.access_token
 
-	def get_response(self, url, count):
-		if count:
-			url = 'https://www.googleapis.com/youtube/v3/' + url
-		status_code = 'Unknown'
+	def try_response(self, url):
 		try:
 			response = compat_ssl_urlopen(url)
 			status_code = response.getcode()
+		except compat_HTTPError as e:
+			print ('[YouTubeApi] HTTPError error in get response')
+			status_code = e.getcode()
 		except:
 			print ('[YouTubeApi] error in get response')
+			status_code = 'Unknown'
 		if status_code == 200:
-			return load(response)
-		elif status_code == 401 and self.access_token and count:
-			self.renew_access_token()
-			self.get_response(url, False)
+			return load(response), None
 		else:
-			print ('[YouTubeApi] error in get response, errorcode', status_code)
-		return {}
+			print ('[YouTubeApi] get response status code', status_code)
+			return {}, status_code
 
-	def get_aut_response(self, method, url, data, header, status, count):
-		url = 'https://www.googleapis.com/youtube/v3/' + url + self.key
-		headers = {'Authorization': 'Bearer %s' % self.access_token}
-		status_code = 'Unknown'
-		if header:
-			headers.update(header)
+	def get_response(self, url):
+		url = 'https://www.googleapis.com/youtube/v3/' + url
+		response, status_code = self.try_response(url)
+		if response:
+			return response
+		elif status_code == 401 and self.access_token:
+			self.renew_access_token()
+			response, status_code = self.try_response(url)
+		return response
+
+	def try_aut_response(self, method, url, data, headers):
 		try:
 			request = compat_Request(url, data=data, headers=headers)
 			request.get_method = lambda: method
 			response = compat_urlopen(request)
 			status_code = response.getcode()
 			response.close()
+		except compat_HTTPError as e:
+			print ('[YouTubeApi] HTTPError error in aut response')
+			status_code = e.getcode()
 		except:
 			print ('[YouTubeApi] error in aut response')
-			return None
+			return 'Unknown'
+		return status_code
+
+	def get_aut_response(self, method, url, data, header, status):
+		url = 'https://www.googleapis.com/youtube/v3/' + url + self.key
+		headers = {'Authorization': 'Bearer %s' % self.access_token}
+		if header:
+			headers.update(header)
+		status_code = self.try_aut_response(method, url, data, header)
+		if status_code == 401 and self.access_token:
+			self.renew_access_token()
+			status_code = self.try_aut_response(method, url, data, header)
 		if status_code == status:
 			return True
-		elif status_code == 401 and self.access_token and count:
-			self.renew_access_token()
-			self.get_aut_response(self, method, url, data, header, status, False)
 		else:
-			print('[YouTubeApi] error in aut response, errorcode', status_code)
+			print('[YouTubeApi] aut response status code', status_code)
 			return None
 
 	def subscriptions_list(self, maxResults, pageToken):
 		pageToken = pageToken and '&pageToken=' + pageToken
 		url = 'subscriptions?part=snippet&maxResults=' + maxResults + \
 			'&mine=true' + pageToken + self.key
-		return self.get_response(url, True)
+		return self.get_response(url)
 
 	def playlists_list(self, maxResults, pageToken):
 		pageToken = pageToken and '&pageToken=' + pageToken
 		url = 'playlists?part=snippet&maxResults=' + maxResults + \
 			'&mine=true' + pageToken + self.key
-		return self.get_response(url, True)
+		return self.get_response(url)
 
 	def channels_list(self, maxResults, pageToken):
 		pageToken = pageToken and '&pageToken=' + pageToken
 		url = 'channels?part=contentDetails&maxResults=' + maxResults + \
 			'&mine=true' + pageToken + self.key
-		return self.get_response(url, True)
+		return self.get_response(url)
 
 	def search_list_full(self, videoEmbeddable, safeSearch, eventType, videoType,
 			videoDefinition, order, part, q, relevanceLanguage,
@@ -123,25 +138,25 @@ class YouTubeApi:
 			videoDefinition + '&order=' + order + '&part=' + part.replace(',', '%2C') + \
 			'&q=' + q + relevanceLanguage + '&type=' + s_type + regionCode + \
 			'&maxResults=' + maxResults + pageToken + self.key
-		return self.get_response(url, True)
+		return self.get_response(url)
 
 	def search_list(self, part, channelId, maxResults, pageToken):
 		pageToken = pageToken and '&pageToken=' + pageToken
 		url = 'search?part=' + part.replace(',', '%2C') + \
 			'&channelId=' + channelId + '&maxResults=' + maxResults + \
 			pageToken + self.key
-		return self.get_response(url, True)
+		return self.get_response(url)
 
 	def videos_list(self, v_id):
 		url = 'videos?part=id%2Csnippet%2Cstatistics%2CcontentDetails&id=' + \
 			v_id.replace(',', '%2C') + self.key
-		return self.get_response(url, True)
+		return self.get_response(url)
 
 	def playlistItems_list(self, maxResults, playlistId, pageToken):
 		pageToken = pageToken and '&pageToken=' + pageToken
 		url = 'playlistItems?part=snippet&maxResults=' + \
 			maxResults + '&playlistId=' + playlistId + pageToken + self.key
-		return self.get_response(url, True)
+		return self.get_response(url)
 
 	def subscriptions_insert(self, channelId):
 		method = 'POST'
@@ -157,17 +172,17 @@ class YouTubeApi:
 			})
 		header = {'content-type': 'application/json'}
 		status = 200
-		return self.get_aut_response(method, url, data, header, status, True)
+		return self.get_aut_response(method, url, data, header, status)
 
 	def subscriptions_delete(self, subscribtionId):
 		method = 'DELETE'
 		url = 'subscriptions?id=' + subscribtionId
 		status = 204
-		return self.get_aut_response(method, url, '', None, status, True)
+		return self.get_aut_response(method, url, '', None, status)
 
 	def videos_rate(self, videoId, rating):
 		method = 'POST'
 		url = 'videos/rate?id=' + videoId + '&rating=' + rating
 		header = {'content-length': '0'}
 		status = 204
-		return self.get_aut_response(method, url, '', header, status, True)
+		return self.get_aut_response(method, url, '', header, status)

src/compat.py
@@ -10,6 +10,7 @@ if version_info[0] == 2:
 	from urllib import unquote as compat_unquote_to_bytes
 	from urllib2 import urlopen as compat_urlopen
 	from urllib2 import Request as compat_Request
+	from urllib2 import HTTPError as compat_HTTPError
 	from urllib2 import URLError as compat_URLError
 	from urlparse import urljoin as compat_urljoin
 	from urlparse import urlparse as compat_urlparse
@@ -21,6 +22,7 @@ else:
 	from urllib.parse import unquote_to_bytes as compat_unquote_to_bytes
 	from urllib.request import urlopen as compat_urlopen
 	from urllib.request import Request as compat_Request
+	from urllib.error import HTTPError as compat_HTTPError
 	from urllib.error import URLError as compat_URLError
 	from urllib.parse import urljoin as compat_urljoin
 	from urllib.parse import urlparse as compat_urlparse

commit 655e282168be672294a0d8754ea45f8d505da289

YouTubeApi: fix headers in get_aut_response
Broken in https://github.com/Taapat/enigma2-plugin-youtube/commit/87aa502a0c2a3b8486f070fe6bba602043b91491

src/YouTubeApi.py
@@ -76,11 +76,11 @@ class YouTubeApi:
 		headers = {'Authorization': 'Bearer %s' % self.access_token}
 		if header:
 			headers.update(header)
-		status_code = self.try_aut_response(method, url, data, header)
+		status_code = self.try_aut_response(method, url, data, headers)
 		if status_code == 401 and self.access_token:
 			print('[YouTubeApi] Unauthorized get aut response, try get new access token')
 			self.renew_access_token()
-			status_code = self.try_aut_response(method, url, data, header)
+			status_code = self.try_aut_response(method, url, data, headers)
 		if status_code == status:
 			return True
 		else: