b6cd86851064a19695d55011c4d641052eaa4d3d
Taapat <taapat@gmail.com>
2021-01-28 22:38:45 +0200

    Perform travis flake8 code check before pytest test session

--- travis_fix.sh
+++ travis_fix.sh
@@ -1,7 +1,7 @@
 # Remove enigma2 python imports and variables in used files
 
 cp -f travis_init.py ./src/__init__.py
-sed -i 's/from Components/#from Components/g' ./src/YouTubeVideoUrl.py
+sed -i 's/from Components/# from Components/g' ./src/YouTubeVideoUrl.py
 sed -i 's/config.plugins.YouTube.maxResolution.value/"22"/g' ./src/YouTubeVideoUrl.py
 sed -i 's/config.plugins.YouTube.searchRegion.value/"US"/g' ./src/YouTubeVideoUrl.py
 sed -i 's/config.plugins.YouTube.searchLanguage.value/"en"/g' ./src/YouTubeVideoUrl.py
