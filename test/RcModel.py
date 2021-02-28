bce9c99d4d993a0a2ba59c52275db916469403da
Taapat <taapat@gmail.com>
2021-02-28 14:51:36 +0200

    Fix RcModel for gui test on atv

--- test/RcModel.py
+++ test/RcModel.py
@@ -13,8 +13,8 @@ class RcModel:
 	def getRcPositions(self):
 		return self.getRcFile('xml')
 
-	def getRcFolder(self):
-		return ''
+	def getRcFolder(self, GetDefault=True, *args):
+		return 'enigma2/data/'
 
 
 rc_model = RcModel()
