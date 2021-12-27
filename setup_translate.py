28185f2495750747f10e5b6a6930a36912e721eb
Madars Auns <taapat@gmail.com>
2021-12-27 11:11:58 +0200

    setup_translate: fix noncompliant raise exception
    skip-release

--- setup_translate.py
+++ setup_translate.py
@@ -35,7 +35,7 @@ class build_trans(cmd.Command):
 						dest = os.path.join(destdir, lang_domain[:-3] + 'mo')
 						print("Language compile %s -> %s" % (src, dest))
 						if os.system("msgfmt '%s' -o '%s'" % (src, dest)) != 0:
-							raise (Exception, "Failed to compile: " + src)
+							raise Exception("Failed to compile", src)
 		else:
 			print("we got no domain -> no translation was compiled")
 
