a5fd2eae5d4640848dc199d3019e06c0c1c77501
Madars Auns <taapat@gmail.com>
2021-12-27 14:26:20 +0200

    setup: use constant instead of duplicating literal
    skip-release

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@ from distutils.core import setup
 import setup_translate
 
 
+PLUGIN_DIR = 'Extensions.YouTube'
+
+
 setup(name='enigma2-plugin-extensions-youtube',
 		version='1.0',
 		author='Taapat',
 		author_email='taapat@gmail.com',
-		package_dir={'Extensions.YouTube': 'src'},
-		packages=['Extensions.YouTube'],
-		package_data={'Extensions.YouTube': ['*.png', 'icons/*.png', '*.svg', 'icons/*.svg']},
+		package_dir={PLUGIN_DIR: 'src'},
+		packages=[PLUGIN_DIR],
+		package_data={PLUGIN_DIR: ['*.png', 'icons/*.png', '*.svg', 'icons/*.svg']},
 		description='Watch YouTube videos',
 		cmdclass=setup_translate.cmdclass)
