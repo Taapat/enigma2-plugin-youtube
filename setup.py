975f3aa5de8a677b122d253c69b365e810e42861
Taapat <taapat@gmail.com>
2022-01-20 12:38:47 +0200

    Fix translation build
    cmdclass is running after copying package_data and therefore it cannot be used to build translations in source.
    Therefore, I created a function in setup which build translation files before copying the source files to the build directory.

--- setup.py
+++ setup.py
@@ -1,8 +1,26 @@
+from __future__ import print_function
 from distutils.core import setup
-import setup_translate
+import os
 
 
 PLUGIN_DIR = 'Extensions.YouTube'
+
+
+def compile_translate():
+	for lang in os.listdir('po'):
+		if lang.endswith('.po'):
+			src = os.path.join('po', lang)
+			lang = lang[:-3]
+			destdir = os.path.join('src/locale', lang, 'LC_MESSAGES')
+			if not os.path.exists(destdir):
+				os.makedirs(destdir)
+			dest = os.path.join(destdir, 'YouTube.mo')
+			print("Language compile %s -> %s" % (src, dest))
+			if os.system("msgfmt '%s' -o '%s'" % (src, dest)) != 0:
+				raise RuntimeError("Failed to compile", src)
+
+
+compile_translate()
 
 
 setup(name='enigma2-plugin-extensions-youtube',
@@ -12,5 +30,4 @@ setup(name='enigma2-plugin-extensions-youtube',
 		package_dir={PLUGIN_DIR: 'src'},
 		packages=[PLUGIN_DIR],
 		package_data={PLUGIN_DIR: ['*.png', 'icons/*.png', '*.svg', 'icons/*.svg', 'locale/*/LC_MESSAGES/*.mo']},
-		description='Watch YouTube videos',
-		cmdclass=setup_translate.cmdclass)
+		description='Watch YouTube videos')
