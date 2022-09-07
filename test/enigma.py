d5c30f88e6837893490486edc020d9ea1cefd321
Madars Auns <taapat@gmail.com>
2022-09-07 13:59:32 +0300

    Fix test on openatv 7 (#176)
    force-test skip-release

--- test/enigma.py
+++ test/enigma.py
@@ -90,6 +90,7 @@ eRCInput = _einstances()
 eRFmod = _einstances()
 eServiceCenter = _einstances()
 eServiceEvent = _einstances()
+eServiceEventEnums = _einstances()
 eSlider = _einstances()
 eStreamServer = _einstances()
 fontRenderClass = _einstances()
@@ -680,6 +681,7 @@ def start_session():
 		config.crash = ConfigSubsection()
 		config.crash.debugActionMaps = ConfigYesNo(default=False)
 		config.crash.debugKeyboards = ConfigYesNo(default=False)
+		config.crash.debugTimers = ConfigYesNo(default=False)
 		config.plugins = ConfigSubsection()
 		from Screens import InfoBar
 	InfoBar.MoviePlayer = new_movie_player
