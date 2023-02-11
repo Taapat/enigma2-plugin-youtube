46586531f35268523a64309b3d9f58da35f765a4
Taapat <taapat@gmail.com>
2023-02-11 16:07:25 +0200

    Improve YouTubeDownload tests
    force-test skip-release

--- test/enigma.py
+++ test/enigma.py
@@ -244,6 +244,12 @@ class eConsoleAppContainer(_einstances):
 		self.dataAvail = []
 		self.appClosed = []
 
+	def execute(self, *cmd):
+		if cmd and 'ffmpeg -i' in cmd[0]:
+			with open('test.mkv', 'w') as fp:
+				pass  # create empty mkv file
+			return True
+
 
 class _eEnv:
 	def resolve(self, path):
