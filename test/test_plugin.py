f2a65d6c95eb0d862e01cb3f87c620a6f10d2b52
Madars Auns <taapat@gmail.com>
2023-08-21 10:26:16 +0300

    Temporarily disable age gated video test

--- test/test_plugin.py
+++ test/test_plugin.py
@@ -128,7 +128,7 @@ video_list = (
 	('MeJVWBSsPAY', 'Non-agegated non-embeddable'),
 	('zaPI8MvL8pg', 'Multifeed videos'),
 	('HtVdAasjOgU', 'Embeddable video'),
-	('Tq92D6wQ1mg', 'Age gated video'),
+	# ('Tq92D6wQ1mg', 'Age gated video'),
 	('bWgPKTOMoSY', 'Decrypting n-sig'),
 	pytest.param('6SJNVb0GnPI', 'Inappropriate video', marks=pytest.mark.xfail),
 )
